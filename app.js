951391309854bc2a197534dcb9e4cbdb515d463c
Imdadwf <100688885+Imdadwf@users.noreply.github.com>
2023-05-22 14:51:01 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+require('dotenv').config();
+
+const HttpError = require('./utils/http-error')
+
+mongoose.set('strictQuery', true)
+
+const app = express()
+app.use(bodyParser.json())
+
+const musiquesRoutes = require('./routes/musiques-routes')
+const filmsRoutes = require('./routes/films-routes')
+
+app.use('/api/musiques', musiquesRoutes);
+app.use('/api/films', filmsRoutes)
+
+app.use((req, res, next) => {
+    const error = new HttpError('Page non trouvée', 404)
+    return next(error)
+})
+
+app.use((error, req, res, next) => {
+    if(res.headerSent) {
+        return next(error)
+    }
+    res.status(error.code || 500);
+    res.json({ message: error.message || 'Une erreur non gérée est survenue'})
+}, )
+
+const uri = 'mongodb+srv://gretaUser:bCx1m4K49VIpKttf@gretalaurent.sdfgski.mongodb.net/Bibliotheque?retryWrites=true&w=majority'
+
+mongoose.connect(uri)
+    .then(() => {
+        app.listen(process.env.PORT);
+        console.log("Server running");
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
